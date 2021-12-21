75b1352225868f7954eb0721c50538ecda115909
monaque <valeriy.drevko@gmail.com>
2021-12-21 19:22:12 +0400

    Lesson1
    Сделал первое домашнее задание

---
+++ Lesson1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW
+{
+    class Lesson1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Как Вас зовут?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Hello, {name}! Сегодня {DateTime.Now}");
+        }
+    }
+}
